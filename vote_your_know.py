f2c861564d1fd09d818bb98a44690cabd63079a4
Will Bastian <will.s.bastian@gmail.com>
2014-09-23 09:26:23 -0400

    inclusion of WTForms cleaned up routing methods (removed redundancy)

--- vote_your_know.py
+++ vote_your_know.py
@@ -4,8 +4,13 @@ import requests
 import json
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import Required
 
 # configuration (tbd/in progress)
+
 DATABASE = '/tmp/votr.db'
 DEBUG = True
 SECRET_KEY = 'development key'
@@ -18,58 +23,65 @@ api_key = "AIzaSyC6WV29UUFLDn4-kXYcwR9Ucuyz1NpYevY"
 election_api_url = "https://www.googleapis.com/civicinfo/v1/elections?key={0}"
 # 0 = election
 # 1 = api_key
-voterinfo_api_url = "https://www.googleapis.com/civicinfo/v1/voterinfo/{0}/lookup?key={1}"
-# This is v2 (updated on 9/19/2014). V2 for voterinfo is not yet workign w/o an election.
-# 0 = address
-# 1 = api_key
-representative_api_url = "https://www.googleapis.com/civicinfo/v2/representatives?address={0}&key={1}"
+voterinfo_api_url = "https://www.googleapis.com/civicinfo/v1/voterinfo/{0}/"\
+    "lookup?key={1}"
 
-print(__name__)
+# This is v2 (updated on 9/19/2014).
+# V2 for voterinfo is not yet workign w/o an election.
+# 0 = address`
+# 1 = api_key
+representative_api_url = "https://www.googleapis.com/civicinfo/v2/"\
+    "representatives?address={0}&key={1}"
+
 # create our little application :)
+print("creating our application....")
 app = Flask(__name__)
 app.config.from_object(__name__)
+print(app.config)
+bootstrap = Bootstrap(app)
 
 
+#############################
+# App methods
+#############################
+
+# routing
 @app.route('/')
 def show_landing_page():
     return render_template('landing_page.html')
 
 
-@app.route('/voterinfo')
+@app.route('/voterinfo', methods=['GET', 'POST'])
 def show_elections():
     print("show_elections")
-    elections = get_elections()
-    return render_template('show_voterinfo.html', elections=elections, voterinfo=None)
+    form = AddressElectionLookup()
+    voterinfo = {}
+    address = None
+    if form.validate_on_submit():
+        address = form.address.data
+        voterinfo = get_voterinfo(form.election.data, address)
+
+    return render_template('show_voterinfo.html',
+                           voterinfo=voterinfo,
+                           lookupform=form)
 
 
-@app.route('/voterinfo', methods=['POST'])
-def lookup_election():
-    elections = get_elections()
-    address = request.form['address']
-    election = request.form['election']
-
-    # flash('New entry was successfully posted')
-    voterinfo = get_voterinfo(election, address)
-    print("election:")
-    print(election)
-    return render_template('show_voterinfo.html', elections=elections,
-                           voterinfo=voterinfo)
-
-
-@app.route('/representatives')
+@app.route('/representatives', methods=['GET', 'POST'])
 def show_representatives():
-    return render_template('show_representativeinfo.html', representatives=None)
+    form = AddressLookup()
+    representatives = {}
+    address = None
+    if form.validate_on_submit():
+        address = form.address.data
+        representatives = get_representativeinfo(address)
+    return render_template('show_representativeinfo.html',
+                           representatives=representatives,
+                           lookupform=form)
 
 
-@app.route('/representatives', methods=['POST'])
-def lookup_representatives():
-    address = request.form['address']
-    print("Address :" + address)
-    representatives = get_representativeinfo(address)
-    return render_template('show_representativeinfo.html', representatives=representatives)
-
-
+# support methods
 def get_elections():
+    # TODO fix this to cover the WTF functionality
     print("get_elections")
     election_api_final = election_api_url.format(api_key)
 
@@ -85,6 +97,19 @@ def get_elections():
         print(key)
 
     return elections
+
+
+def get_elections_wtf():
+    elections = get_elections()
+    election_tuples = []
+    print("ELECTIONS")
+    print(elections)
+    for key, value in elections.items():
+        election_tuples.append((key, value['name']))
+
+    print("ELECTION TUPLES")
+    print(election_tuples)
+    return election_tuples
 
 
 def get_voterinfo(election, address):
@@ -111,14 +136,22 @@ def get_representativeinfo(address):
 
     return json_respresentatives
 
+
+# form classes
+class AddressElectionLookup(Form):
+    """lookup class for the election/address tuple"""
+    address = StringField('Address:', validators=[Required()])
+    elections = get_elections_wtf()
+    election = SelectField('Election: ', choices=elections)
+    submit = SubmitField('Submit')
+
+
+class AddressLookup(Form):
+    """lookup class for the address only"""
+    address = StringField('Address:', validators=[Required()])
+    submit = SubmitField('Submit')
+
 # must go at end!
 if __name__ == '__main__':
     print ('starting!')
     app.run(debug=True)
-
-# selection = input('Select Election:')
-
-# voterinfo_api_final = voterinfo_api_url.format(selection, api_key)
-# print(voterinfo_api_final)
-
-# address = input('Enter Address (street, city, state, zip):')
