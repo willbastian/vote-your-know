be89cad1aedac87089ac0a38e1121c49205ab2a8
Will Bastian <will.s.bastian@gmail.com>
2014-10-12 14:52:59 -0400

    testing further renames

--- manage.py
+++ manage.py
@@ -5,12 +5,12 @@ from application.models import User, Role
 from flask.ext.script import Manager, Shell
 
 # create_app defined in app (see imports)
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
+application = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(application)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+    return dict(app=application, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
